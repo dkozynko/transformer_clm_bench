d93fb51504558d1f2bf8957dbf03e23687d7ba7f
Dmytro Kozynko <shamadei@gmail.com>
2026-03-31 22:48:53 +0200

    feat: add byte-level tokenization

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,4 +1,4 @@
-from transformer_clm_bench.data import build_vocabulary, encode_tokens
+from transformer_clm_bench.data import build_vocabulary, decode_token_ids, encode_text, encode_tokens
 
 
 def test_build_vocabulary_and_encoding_cover_special_tokens():
@@ -6,3 +6,9 @@ def test_build_vocabulary_and_encoding_cover_special_tokens():
     ids = encode_tokens(["<bos>", "hello", "<eos>"], vocab)
     assert len(vocab) >= 5
     assert len(ids) == 3
+
+
+def test_byte_tokenization_round_trips_readable_text():
+    token_ids = encode_text("Hello byte world!", tokenizer_name="byte")
+    decoded = decode_token_ids(token_ids, tokenizer_name="byte")
+    assert decoded == "Hello byte world!"
